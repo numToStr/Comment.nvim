6a737deec1ecffc94dd74619ef7b4cb9cc8b7321
numtostr <vikasraj1911@gmail.com>
2021-10-03 13:54:41 +0530

    wip

--- lua/Comment/lang.lua
+++ lua/Comment/lang.lua
@@ -1,21 +1,40 @@
----Lang table
----@class lang_table
+---Common commentstring used in mutliple languages
+local M = {
+    cxx_ln = '//%s',
+    cxx_bl = '/*%s*/',
+    hash = '#%s',
+}
+
+---Lang table that contains commentstring (linewise/blockwise) for mutliple filetypes
+---@type table { filetype = { linewise = string, blockwise = string|nil } }
 local L = {
-    toml = '#%s',
+    toml = { M.hash },
+    javascript = { M.cxx_ln, M.cxx_bl },
 }
 
 return setmetatable({}, {
     __index = {
+        set = function(k, v)
+            if type(v) == 'string' then
+                v = { v }
+            end
+            L[k] = v
+        end,
         get = function(lang)
-            return L[lang]
+            local l = L[lang]
+            return l and l[1]
+        end,
+        get_bl = function(lang)
+            local l = L[lang]
+            return l and l[2]
         end,
     },
-    __newindex = function(_, k, v)
-        L[k] = v
+    __newindex = function(this, k, v)
+        this.set(k, v)
     end,
-    __call = function(_, langs, spec)
+    __call = function(this, langs, spec)
         for _, lang in ipairs(langs) do
-            L[lang] = spec
+            this.set(lang, spec)
         end
     end,
 })
