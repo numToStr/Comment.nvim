549491fa87408dec0978c7a1c807d866b215261a
numtostr <vikasraj1911@gmail.com>
2021-10-05 15:14:28 +0530

    feat: ignore lines

--- lua/Comment/init.lua
+++ lua/Comment/init.lua
@@ -1,27 +1,8 @@
-local U = require('Comment.utils')
 local C = require('Comment.comment')
 
-local M = {
+return {
     setup = C.setup,
-    toggle = C.toggle_ln,
+    toggle = C.toggle,
+    comment = C.comment,
+    uncomment = C.uncomment,
 }
-
----Comments the current line
-function M.comment()
-    local rcs, lcs = C.unwrap_cstr()
-    local l = vim.api.nvim_get_current_line()
-
-    C.comment_ln(l, rcs, lcs)
-    U.is_hook(C.config.post_hook, -1)
-end
-
----Unomments the current line
-function M.uncomment()
-    local rcs, lcs = C.unwrap_cstr()
-    local line = vim.api.nvim_get_current_line()
-
-    C.uncomment_ln(line, vim.pesc(rcs), vim.pesc(lcs))
-    U.is_hook(C.config.post_hook, -1)
-end
-
-return M
