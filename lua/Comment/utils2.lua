8984974781d80ef932dfe331b8e123c7e0a74be1
numtostr <vikasraj1911@gmail.com>
2021-10-19 12:51:13 +0530

    ok docs

--- lua/Comment/utils2.lua
+++ lua/Comment/utils2.lua
@@ -2,20 +2,39 @@ local A = vim.api
 
 local U = {}
 
-function U.is_start_commented(s, lcs_esc)
-    return s:find('^%s*(' .. lcs_esc .. ')')
+---Check if the start of string is commented
+---@param str string String to be checked
+---@param lcs_esc string Escaped LHS of commentring
+---@return number number Start index of the match (Mostly will be 1)
+---@return number number End index of the match
+function U.is_start_commented(str, lcs_esc)
+    return str:find('^%s*' .. lcs_esc)
 end
 
-function U.is_end_commented(s, rcs_esc)
-    return s:find('(' .. rcs_esc .. ')$')
+---Check if the end of string is commented
+---@param str string String to be checked
+---@param rcs_esc string Escaped LHS of commentring
+---@return number number Start index of the match
+---@return number number End index of the match
+function U.is_end_commented(str, rcs_esc)
+    return str:find(rcs_esc .. '$')
 end
 
-function U.rm_comment(row, start_col, end_col)
-    A.nvim_buf_set_text(0, row, start_col, row, end_col, {})
+---Removes the comment chars from the string
+---@param row number Row/Line number
+---@param start_idx number Start index of column
+---@param end_idx number End index of column
+function U.rm_comment(row, start_idx, end_idx)
+    A.nvim_buf_set_text(0, row, start_idx, row, end_idx, {})
 end
 
-function U.add_comment(row, start_col, end_col, cstr)
-    A.nvim_buf_set_text(0, row, start_col, row, end_col, { cstr })
+---Adds the comment chars from the string
+---@param row number Row/Line number
+---@param start_idx number Start index of column
+---@param end_idx number End index of column
+---@param chars string LHS/RHS of commentring
+function U.add_comment(row, start_idx, end_idx, chars)
+    A.nvim_buf_set_text(0, row, start_idx, row, end_idx, { chars })
 end
 
 return U
