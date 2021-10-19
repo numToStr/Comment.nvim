8bc578efc405900981b196cccc7b4a38431bddaf
numtostr <vikasraj1911@gmail.com>
2021-10-19 18:48:33 +0530

    I made some progress

--- lua/Comment/opfunc2.lua
+++ lua/Comment/opfunc2.lua
@@ -4,13 +4,12 @@ local U = require('Comment.utils')
 local O = {}
 
 -- FIXME support RHS of commentstring
--- FIXME uncommenting of line which was empty before
 ---Linewise commenting
 ---@param p OpFnParams
 ---@return integer CMode
 function O.linewise(p)
-    local padding, pad_patrn = U.get_padding(p.cfg.padding)
-    local lcs_esc, _ = p.lcs and vim.pesc(p.lcs) .. pad_patrn, p.rcs and pad_patrn .. vim.pesc(p.rcs)
+    local padding, pp = U.get_padding(p.cfg.padding)
+    local lcs_esc, _ = U.escape(p.lcs), U.escape(p.rcs)
 
     -- While commenting a block of text, there is a possiblity of lines being both commented and non-commented
     -- In that case, we need to figure out that if any line is uncommented then we should comment the whole block or vise-versa
@@ -27,10 +26,10 @@ function O.linewise(p)
     -- If the given comde is uncomment then we actually don't want to compute the cmode or min_indent
     if p.cmode ~= U.cmode.uncomment then
         for _, line in ipairs(p.lines) do
-            -- I wish lua had `continue` statement [sad noises]
             if not U.ignore(line, pattern) then
+                -- I wish lua had `continue` statement [sad noises]
                 if cmode == U.cmode.uncomment and p.cmode == U.cmode.toggle then
-                    local is_cmt, col_end_idx = U2.is_start_commented(line, lcs_esc)
+                    local is_cmt, col_end_idx = U2.is_start_commented(line, lcs_esc, pp)
                     if not is_cmt then
                         cmode = U.cmode.comment
                     else
@@ -49,7 +48,7 @@ function O.linewise(p)
 
                 -- In case of uncomment we might only need indent of first line
                 if not min_col and (cmode == U.cmode.uncomment or p.cmode == U.cmode.uncomment) then
-                    min_col = U.grab_indent(line)
+                    min_col, min_space = U.grab_indent(line)
                 end
             end
         end
@@ -63,12 +62,19 @@ function O.linewise(p)
     local uncomment = cmode == U.cmode.uncomment
     for i, line in ipairs(p.lines) do
         if not U.ignore(line, pattern) then
+            -- I wish lua had `continue` statement [sad noises]
             local srow = p.srow + i - 2
             if uncomment then
-                U2.rm_comment(srow, min_col, end_of_lcs)
+                local maybe_len = min_col + #p.lcs
+                if maybe_len == #line then
+                    -- This line was previously empty
+                    U2.rm_comment(srow, 0, maybe_len)
+                else
+                    U2.rm_comment(srow, min_col, end_of_lcs)
+                end
             else
                 if U.is_empty(line) then
-                    U2.add_comment(srow, 0, 0, min_space .. p.lcs)
+                    U2.add_comment(srow, 0, 0, (min_space or '') .. p.lcs)
                 else
                     U2.add_comment(srow, min_col, min_col, p.lcs .. padding)
                 end
@@ -87,16 +93,15 @@ end
 function O.blockwise(p, partial)
     -- Block wise, only when there are more than 1 lines
     local sln, eln = p.lines[1], p.lines[2]
-    local padding = U.get_padding(p.cfg.padding)
-    local lcs, rcs = p.lcs .. padding, padding .. p.rcs
-    local lcs_esc, rcs_esc = U.escape(lcs), U.escape(rcs)
+    local padding, pp = U.get_padding(p.cfg.padding)
+    local lcs_esc, rcs_esc = U.escape(p.lcs), U.escape(p.rcs)
 
     local cmode, scol_s, scol_e, ecol_s, ecol_e
 
     -- If given mode is toggle then determine whether to comment or not
     if p.cmode == U.cmode.toggle then
-        scol_s, scol_e = U2.is_start_commented(sln, lcs_esc)
-        ecol_s, ecol_e = U2.is_end_commented(eln, rcs_esc)
+        scol_s, scol_e = U2.is_start_commented(sln, lcs_esc, pp)
+        ecol_s, ecol_e = U2.is_end_commented(eln, rcs_esc, pp)
 
         cmode = (scol_s and ecol_s) and U.cmode.uncomment or U.cmode.comment
     else
@@ -116,8 +121,8 @@ function O.blockwise(p, partial)
     end
 
     local eln_len = #eln
-    U2.add_comment(srow, space_len, space_len, lcs)
-    U2.add_comment(erow, eln_len, eln_len, rcs)
+    U2.add_comment(srow, space_len, space_len, p.lcs .. padding)
+    U2.add_comment(erow, eln_len, eln_len, padding .. p.rcs)
 
     return cmode
 end
--- lua/Comment/utils2.lua
+++ lua/Comment/utils2.lua
@@ -5,19 +5,21 @@ local U = {}
 ---Check if the start of string is commented
 ---@param str string String to be checked
 ---@param lcs_esc string Escaped LHS of commentring
+---@param pp string Padding pattern
 ---@return number number Start index of the match (Mostly will be 1)
 ---@return number number End index of the match
-function U.is_start_commented(str, lcs_esc)
-    return str:find('^%s*' .. lcs_esc)
+function U.is_start_commented(str, lcs_esc, pp)
+    return str:find(lcs_esc and '^%s*' .. lcs_esc .. pp or '')
 end
 
 ---Check if the end of string is commented
 ---@param str string String to be checked
 ---@param rcs_esc string Escaped LHS of commentring
+---@param pp string Padding pattern
 ---@return number number Start index of the match
 ---@return number number End index of the match
-function U.is_end_commented(str, rcs_esc)
-    return str:find(rcs_esc .. '$')
+function U.is_end_commented(str, rcs_esc, pp)
+    return str:find(rcs_esc and pp .. rcs_esc .. '$' or '')
 end
 
 ---Removes the comment chars from the string
