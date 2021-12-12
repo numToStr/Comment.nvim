f1cc8e22d2c3b98ef0afa58027b9013682e31528
numToStr <vikasraj1911@gmail.com>
2021-12-12 20:05:38 +0530

    remove old api

--- lua/Comment/init.lua
+++ lua/Comment/init.lua
@@ -1,9 +1,5 @@
-local C = require('Comment.api')
-
 return {
-    setup = C.setup,
-    toggle = C.toggle,
-    comment = C.comment,
-    uncomment = C.uncomment,
-    get_config = C.get_config,
+    setup = function(...)
+        require('Comment.api').setup(...)
+    end,
 }
