136711aa93ccde158b53cbf1cbf227ae32106e55
numToStr <vikasraj1911@gmail.com>
2021-12-20 15:54:52 +0530

    fixed

--- after/plugin/Comment.lua
+++ after/plugin/Comment.lua
@@ -16,7 +16,7 @@ K.n(
 
 -- Operator-Pending mappings
 K.n('<Plug>(comment_toggle_linewise)', '<CMD>lua require("Comment.api").call("toggle_linewise_op")<CR>g@')
-K.n('<Plug>(comment_toggle_blockwise)', '<CMD>lua require("Comment.api").call("toggle_current_blockwise_op")<CR>g@')
+K.n('<Plug>(comment_toggle_blockwise)', '<CMD>lua require("Comment.api").call("toggle_blockwise_op")<CR>g@')
 
 -- Visual-Mode mappings
 K.x(
