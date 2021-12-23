29f4452744f8d16fcc494c035b563ed0e8ba1b65
numToStr <vikasraj1911@gmail.com>
2021-12-23 16:04:39 +0530

    ok

--- after/plugin/Comment.lua
+++ after/plugin/Comment.lua
@@ -1,29 +1,29 @@
-local K = require('Comment.utils').K
+local U = require('Comment.utils')
 
 -- Count mappings
-K.n('<Plug>(comment_toggle_linewise_count)', '<CMD>lua require("Comment.api").toggle_linewise_count()<CR>')
-K.n('<Plug>(comment_toggle_blockwise_count)', '<CMD>lua require("Comment.api").toggle_blockwise_count()<CR>')
+U.K.n('<Plug>(comment_toggle_linewise_count)', '<CMD>lua require("Comment.api").toggle_linewise_count()<CR>')
+U.K.n('<Plug>(comment_toggle_blockwise_count)', '<CMD>lua require("Comment.api").toggle_blockwise_count()<CR>')
 
 -- Toggle mappings
-K.n(
+U.K.n(
     '<Plug>(comment_toggle_current_linewise)',
     '<CMD>lua require("Comment.api").call("toggle_current_linewise_op")<CR>g@$'
 )
-K.n(
+U.K.n(
     '<Plug>(comment_toggle_current_blockwise)',
     '<CMD>lua require("Comment.api").call("toggle_current_blockwise_op")<CR>g@$'
 )
 
 -- Operator-Pending mappings
-K.n('<Plug>(comment_toggle_linewise)', '<CMD>lua require("Comment.api").call("toggle_linewise_op")<CR>g@')
-K.n('<Plug>(comment_toggle_blockwise)', '<CMD>lua require("Comment.api").call("toggle_blockwise_op")<CR>g@')
+U.K.n('<Plug>(comment_toggle_linewise)', '<CMD>lua require("Comment.api").call("toggle_linewise_op")<CR>g@')
+U.K.n('<Plug>(comment_toggle_blockwise)', '<CMD>lua require("Comment.api").call("toggle_blockwise_op")<CR>g@')
 
 -- Visual-Mode mappings
-K.x(
+U.K.x(
     '<Plug>(comment_toggle_linewise_visual)',
     '<ESC><CMD>lua require("Comment.api").toggle_linewise_op(vim.fn.visualmode())<CR>'
 )
-K.x(
+U.K.x(
     '<Plug>(comment_toggle_blockwise_visual)',
     '<ESC><CMD>lua require("Comment.api").toggle_blockwise_op(vim.fn.visualmode())<CR>'
 )
