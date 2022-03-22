2fb61933d840575926b9ec55f0e4d15175bbc7f8
Melker Österberg <melkerosterberg@gmail.com>
2022-03-22 22:43:07 +0100

    Set the correct indentation with gcA empty lines

--- lua/Comment/extra.lua
+++ lua/Comment/extra.lua
@@ -66,27 +66,26 @@ function E.insert_eol(ctype, cfg)
     local line = A.nvim_get_current_line()
     local padding = U.get_padding(cfg.padding)
 
-    -- NOTE:
-    -- 1. If the line is empty, don't put any space in front of the comment
-    -- 2. Python is the only language that recommends 2 spaces between the statement and the comment
-    -- 3. Other than that, I am assuming that the users wants a space b/w the end of line and start of the comment
-    local space
     if line:match('^$') then
-        space = ''
-    elseif vim.bo.filetype == 'python' then
-        space = '  '
+        -- If line is empty, start comment at the correct indentation level
+        A.nvim_buf_set_lines(0, srow - 1, srow, false, { lcs .. padding })
+        vim.cmd('normal! ==')
+        U.move_n_insert(srow, A.nvim_get_current_line():len())
     else
-        space = ' '
+        -- NOTE:
+        -- 1. Python is the only language that recommends 2 spaces between the statement and the comment
+        -- 2. Other than that, I am assuming that the users wants a space b/w the end of line and start of the comment
+        local space = vim.bo.filetype == 'python' and '  ' or ' '
+        local ll = line .. space .. lcs .. padding
+
+        -- We need RHS of cstr, if we are doing block comments or if RHS exists
+        -- because even in line comment RHS do exists for some filetypes like jsx_element, ocaml
+        local if_rcs = (ctype == U.ctype.block or rcs) and padding .. rcs or ''
+
+        local erow, ecol = srow - 1, #ll - 1
+        A.nvim_buf_set_lines(0, erow, srow, false, { ll .. if_rcs })
+        U.move_n_insert(srow, ecol)
     end
-    local ll = line .. space .. lcs .. padding
-
-    -- We need RHS of cstr, if we are doing block comments or if RHS exists
-    -- because even in line comment RHS do exists for some filetypes like jsx_element, ocaml
-    local if_rcs = (ctype == U.ctype.block or rcs) and padding .. rcs or ''
-
-    local erow, ecol = srow - 1, #ll - 1
-    A.nvim_buf_set_lines(0, erow, srow, false, { ll .. if_rcs })
-    U.move_n_insert(srow, ecol)
     U.is_fn(cfg.post_hook, ctx)
 end
 
