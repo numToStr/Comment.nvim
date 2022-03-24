85119fea7485df8e39ddeaf5271c7a10d69bddee
Melker Österberg <melkerosterberg@gmail.com>
2022-03-24 14:48:45 +0100

    fix(insert_eol): don't add space in front of comment on empty lines (#131)
    also set the correct indentation with on empty lines

--- lua/Comment/extra.lua
+++ lua/Comment/extra.lua
@@ -66,18 +66,27 @@ function E.insert_eol(ctype, cfg)
     local line = A.nvim_get_current_line()
     local padding = U.get_padding(cfg.padding)
 
-    -- NOTE:
-    -- 1. Python is the only language that recommends 2 spaces between the statement and the comment
-    -- 2. Other than that, I am assuming that the users wants a space b/w the end of line and start of the comment
-    local space = vim.bo.filetype == 'python' and '  ' or ' '
-    local ll = line .. space .. lcs .. padding
-
     -- We need RHS of cstr, if we are doing block comments or if RHS exists
     -- because even in line comment RHS do exists for some filetypes like jsx_element, ocaml
     local if_rcs = (ctype == U.ctype.block or rcs) and padding .. rcs or ''
 
-    local erow, ecol = srow - 1, #ll - 1
-    A.nvim_buf_set_lines(0, erow, srow, false, { ll .. if_rcs })
+    local ecol
+    if line:match('^$') then
+        -- If line is empty, start comment at the correct indentation level
+        A.nvim_buf_set_lines(0, srow - 1, srow, false, { lcs .. padding .. if_rcs})
+        vim.cmd('normal! ==')
+        ecol = A.nvim_get_current_line():len() - if_rcs:len() - 1
+    else
+        -- NOTE:
+        -- 1. Python is the only language that recommends 2 spaces between the statement and the comment
+        -- 2. Other than that, I am assuming that the users wants a space b/w the end of line and start of the comment
+        local space = vim.bo.filetype == 'python' and '  ' or ' '
+        local ll = line .. space .. lcs .. padding
+
+        local erow = srow - 1
+        ecol = #ll - 1
+        A.nvim_buf_set_lines(0, erow, srow, false, { ll .. if_rcs })
+    end
     U.move_n_insert(srow, ecol)
     U.is_fn(cfg.post_hook, ctx)
 end
