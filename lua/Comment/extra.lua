03b2a8f81102f2994f4888760e0f08385d841c3f
numToStr <vikasraj1911@gmail.com>
2022-03-25 17:19:45 +0530

    chore: some random stuff

--- lua/Comment/extra.lua
+++ lua/Comment/extra.lua
@@ -68,14 +68,14 @@ function E.insert_eol(ctype, cfg)
 
     -- We need RHS of cstr, if we are doing block comments or if RHS exists
     -- because even in line comment RHS do exists for some filetypes like jsx_element, ocaml
-    local if_rcs = (ctype == U.ctype.block or rcs) and padding .. rcs or ''
+    local if_rcs = rcs and padding .. rcs or ''
 
     local ecol
-    if line:match('^$') then
+    if U.is_empty(line) then
         -- If line is empty, start comment at the correct indentation level
-        A.nvim_buf_set_lines(0, srow - 1, srow, false, { lcs .. padding .. if_rcs})
-        vim.cmd('normal! ==')
-        ecol = A.nvim_get_current_line():len() - if_rcs:len() - 1
+        A.nvim_buf_set_lines(0, srow - 1, srow, false, { lcs .. padding .. if_rcs })
+        A.nvim_command('normal! ==')
+        ecol = #A.nvim_get_current_line() - #if_rcs - 1
     else
         -- NOTE:
         -- 1. Python is the only language that recommends 2 spaces between the statement and the comment
@@ -83,10 +83,10 @@ function E.insert_eol(ctype, cfg)
         local space = vim.bo.filetype == 'python' and '  ' or ' '
         local ll = line .. space .. lcs .. padding
 
-        local erow = srow - 1
         ecol = #ll - 1
-        A.nvim_buf_set_lines(0, erow, srow, false, { ll .. if_rcs })
+        A.nvim_buf_set_lines(0, srow - 1, srow, false, { ll .. if_rcs })
     end
+
     U.move_n_insert(srow, ecol)
     U.is_fn(cfg.post_hook, ctx)
 end
