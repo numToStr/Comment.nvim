13a5b63066dd2018744f87f9492ba1c9ee1a0577
Mat Jones <mat@mjones.network>
2022-10-13 11:36:51 +0000

    Update lua/Comment/ft.lua
    Co-authored-by: numToStr <24727447+numToStr@users.noreply.github.com>

--- lua/Comment/ft.lua
+++ lua/Comment/ft.lua
@@ -36,7 +36,7 @@ local M = {
 ---@type table<string,string[]>
 local L = setmetatable({
     arduino = { M.cxx_l, M.cxx_b },
-    applescript = { '#%s' },
+    applescript = { M.hash },
     bash = { M.hash },
     bib = { M.latex },
     c = { M.cxx_l, M.cxx_b },
