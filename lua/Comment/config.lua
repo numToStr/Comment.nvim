numToStr/Comment.nvim

commit 2a165144ffdf6e1fbe7127f39cd185d5d111927f

chore!: error handling (#285)
This adds error handling to the plugin, implemented with the help of
`assert` and `xpcall`. Adding explicit error handling for the following
cases for now:

- Invalid commenstring / Commenting is not supported like `json`
- When filetype doesn't support block comments like `yaml`
- Graceful handling of certain cases like uncommenting when there is
nothing to uncomment.

---

fixes #221 
supersedes #277

lua/Comment/ft.lua
@@ -193,7 +193,12 @@ function ft.get(lang, ctype)
     if not ctype then
         return vim.deepcopy(tuple)
     end
-    return tuple[ctype]
+    local cmt_str = tuple[ctype]
+    assert(
+        cmt_str or (ctype ~= require('Comment.utils').ctype.blockwise),
+        { msg = lang .. " doesn't support block comments!" }
+    )
+    return cmt_str
 end
 
 ---Get a language tree for a given range by walking the parse tree recursively.

lua/Comment/utils.lua
@@ -1,6 +1,5 @@
 ---@mod comment.utils Utilities
 
-local F = require('Comment.ft')
 local A = vim.api
 
 local U = {}
@@ -89,7 +88,7 @@ end
 
 ---@private
 ---Call a function if exists
----@param fn unknown|fun():unknown Wanna be function
+---@param fn unknown|fun(...):unknown Wanna be function
 ---@return unknown
 function U.is_fn(fn, ...)
     if type(fn) == 'function' then
@@ -156,7 +155,7 @@ function U.unwrap_cstr(cstr)
 
     assert(
         (left or right),
-        string.format("[Comment] Invalid commentstring - %q. Run ':h commentstring' for help.", cstr)
+        { msg = string.format('Invalid commentstring for %s! Read `:h commentstring` for help.', vim.bo.filetype) }
     )
 
     return vim.trim(left), vim.trim(right)
@@ -174,7 +173,7 @@ function U.parse_cstr(cfg, ctx)
     -- 1. We ask `pre_hook` for a commentstring
     local cstr = U.is_fn(cfg.pre_hook, ctx)
         -- 2. Calculate w/ the help of treesitter
-        or F.calculate(ctx)
+        or require('Comment.ft').calculate(ctx)
         -- 3. Last resort to use native commentstring
         or vim.bo.commentstring
 
@@ -211,8 +210,8 @@ function U.commenter(left, right, padding, scol, ecol, tabbed)
             if scol == 0 then
                 return (ll .. line .. rr)
             end
-            local first = string.sub(line, 0, scol)
-            local last = string.sub(line, scol + 1, -1)
+            local first = string.sub(line --[[@as string]], 0, scol)
+            local last = string.sub(line --[[@as string]], scol + 1, -1)
             return first .. ll .. last .. rr
         end
 
@@ -294,6 +293,8 @@ function U.uncommenter(left, right, padding, scol, ecol)
         ------------------
         if is_lw then
             local a, b, c = string.match(line, pattern)
+            -- When user tries to uncomment when there is nothing to uncomment. See #221
+            assert(a and b, { msg = 'Nothing to uncomment!' })
             -- If there is nothing after LHS then just return ''
             -- bcz the line previously (before comment) was empty
             return U.is_empty(b) and b or a .. b .. (c or '')
@@ -357,6 +358,15 @@ function U.is_commented(left, right, padding, scol, ecol)
         -- SOURCE: https://github.com/numToStr/Comment.nvim/issues/224
         return string.find(string.sub(line, scol + 1, (ecol > #line and #line or ecol + 1)), pattern) ~= nil
     end
+end
+
+---@private
+---Error handler
+---@param ... unknown
+function U.catch(fn, ...)
+    xpcall(fn, function(err)
+        vim.notify(string.format('[Comment.nvim] %s', err.msg), vim.log.levels.WARN)
+    end, ...)
 end
 
 return U

lua/Comment/api.lua
@@ -13,6 +13,10 @@ local A = vim.api
 
 local api, core = {}, {}
 
+---API metamethods
+---@param that table
+---@param ctype CommentType
+---@return table
 function core.__index(that, ctype)
     local idxd = {}
     local mode, type = that.cmode, U.ctype[ctype]
@@ -22,17 +26,17 @@ function core.__index(that, ctype)
     ---In current-line linewise method, 'opmode' is not useful which is always equals to `char`
     ---but we need 'nil' here which is used for current-line
     function idxd.current(_, cfg)
-        Op.opfunc(nil, cfg or Config:get(), mode, type)
+        U.catch(Op.opfunc, nil, cfg or Config:get(), mode, type)
     end
 
     ---To comment lines with a count
     function idxd.count(count, cfg)
-        Op.count(count or A.nvim_get_vvar('count'), cfg or Config:get(), mode, type)
+        U.catch(Op.count, count or A.nvim_get_vvar('count'), cfg or Config:get(), mode, type)
     end
 
     ---@private
     ---To comment lines with a count, also dot-repeatable
-    ---WARNING: This is not part of the API but anyone case use it, if they want
+    ---WARN: This is not part of the API but anyone case use it, if they want
     function idxd.count_repeat(_, count, cfg)
         idxd.count(count, cfg)
     end
@@ -40,7 +44,7 @@ function core.__index(that, ctype)
     return setmetatable({}, {
         __index = idxd,
         __call = function(_, motion, cfg)
-            Op.opfunc(motion, cfg or Config:get(), mode, type)
+            U.catch(Op.opfunc, motion, cfg or Config:get(), mode, type)
         end,
     })
 end
@@ -173,13 +177,13 @@ api.insert = setmetatable({}, {
     __index = function(_, ctype)
         return {
             above = function(cfg)
-                Ex.insert_above(U.ctype[ctype], cfg or Config:get())
+                U.catch(Ex.insert_above, U.ctype[ctype], cfg or Config:get())
             end,
             below = function(cfg)
-                Ex.insert_below(U.ctype[ctype], cfg or Config:get())
+                U.catch(Ex.insert_below, U.ctype[ctype], cfg or Config:get())
             end,
             eol = function(cfg)
-                Ex.insert_eol(U.ctype[ctype], cfg or Config:get())
+                U.catch(Ex.insert_eol, U.ctype[ctype], cfg or Config:get())
             end,
         }
     end,

lua/Comment/opfunc.lua
@@ -171,14 +171,14 @@ function Op.linewise(param)
         local uncomment = U.uncommenter(param.lcs, param.rcs, padding)
         for i, line in ipairs(param.lines) do
             if not U.ignore(line, pattern) then
-                param.lines[i] = uncomment(line)
+                param.lines[i] = uncomment(line) --[[@as string]]
             end
         end
     else
         local comment = U.commenter(param.lcs, param.rcs, padding, min_indent, nil, tabbed)
         for i, line in ipairs(param.lines) do
             if not U.ignore(line, pattern) then
-                param.lines[i] = comment(line)
+                param.lines[i] = comment(line) --[[@as string]]
             end
         end
     end

commit 5efd8f65b8ce0224d2339fea42bc1900d7a40795

fix!: don't prevent block comment in nested filetype
This fixes a bug introduced by `2a16514`, where it prevents doing block
comments in nested filetype (i.e. `lua`), if parent language doesn't
support block comments (i.e. `vim`).

lua/Comment/config.lua
@@ -4,32 +4,18 @@
 ---Following is the default config for the |comment.usage.setup|. If you want to
 ---override, just modify the option that you want, then it will be merged with the
 ---default config.
----
 --->
----    {
----        padding = true,
----        sticky = true,
----        ignore = nil,
----        toggler = {
----            line = 'gcc',
----            block = 'gbc',
----        },
----        opleader = {
----            line = 'gc',
----            block = 'gb',
----        },
----        extra = {
----            above = 'gcO',
----            below = 'gco',
----            eol = 'gcA',
----        },
----        mappings = {
----            basic = true,
----            extra = true,
----        },
----        pre_hook = nil,
----        post_hook = nil,
----    }
+---  {
+---      padding = true,
+---      sticky = true,
+---      ignore = nil,
+---      toggler = { line = 'gcc', block = 'gbc' },
+---      opleader = { line = 'gc', block = 'gb' },
+---      extra = { above = 'gcO', below = 'gco', eol = 'gcA' },
+---      mappings = { basic = true, extra = true },
+---      pre_hook = nil,
+---      post_hook = nil,
+---  }
 ---<
 ---@brief ]]
 

lua/Comment/ft.lua
@@ -170,7 +170,8 @@ end
 ---@param lang string Filetype/Language of the buffer
 ---@param ctype? integer See |comment.utils.ctype|. If given `nil`, it'll
 ---return a copy of { line, block } commentstring.
----@return nil|string|string[] #Commentstring
+---@return nil|string|string[] #Returns stored commentstring, if {lang} is not
+---recognized then returns native |commentstring|
 ---@usage [[
 ---local ft = require('Comment.ft')
 ---local U = require('Comment.utils')
@@ -181,24 +182,19 @@ end
 ---
 ----- 2. Compound filetype
 ----- NOTE: This will return `yaml` commenstring(s),
------       as `ansible` commentstring doesn't exists yet.
+-----       as `ansible` commentstring is not found.
 ---ft.get('ansible.yaml', U.ctype.linewise) -- `#%s`
 ---ft.get('ansible.yaml') -- { '#%s' }
 ---@usage ]]
 function ft.get(lang, ctype)
     local tuple = L[lang]
     if not tuple then
-        return nil
+        return vim.bo.commentstring
     end
     if not ctype then
         return vim.deepcopy(tuple)
     end
-    local cmt_str = tuple[ctype]
-    assert(
-        cmt_str or (ctype ~= require('Comment.utils').ctype.blockwise),
-        { msg = lang .. " doesn't support block comments!" }
-    )
-    return cmt_str
+    return tuple[ctype]
 end
 
 ---Get a language tree for a given range by walking the parse tree recursively.
@@ -236,10 +232,9 @@ end
 function ft.calculate(ctx)
     local buf = A.nvim_get_current_buf()
     local ok, parser = pcall(vim.treesitter.get_parser, buf)
-    local default = ft.get(A.nvim_buf_get_option(buf, 'filetype'), ctx.ctype)
 
     if not ok then
-        return default --[[ @as string ]]
+        return ft.get(vim.bo.filetype, ctx.ctype) --[[ @as string ]]
     end
 
     local lang = ft.contains(parser, {
@@ -249,7 +244,7 @@ function ft.calculate(ctx)
         ctx.range.ecol,
     }):lang()
 
-    return ft.get(lang, ctx.ctype) or default --[[ @as string ]]
+    return ft.get(lang, ctx.ctype) or ft.get(vim.bo.filetype, ctx.ctype) --[[ @as string ]]
 end
 
 ---@export ft

lua/Comment/utils.lua
@@ -1,5 +1,6 @@
 ---@mod comment.utils Utilities
 
+local F = require('Comment.ft')
 local A = vim.api
 
 local U = {}
@@ -171,13 +172,16 @@ end
 ---@return string string Right side of the commentstring
 function U.parse_cstr(cfg, ctx)
     -- 1. We ask `pre_hook` for a commentstring
-    local cstr = U.is_fn(cfg.pre_hook, ctx)
+    local inbuilt = U.is_fn(cfg.pre_hook, ctx)
         -- 2. Calculate w/ the help of treesitter
-        or require('Comment.ft').calculate(ctx)
-        -- 3. Last resort to use native commentstring
-        or vim.bo.commentstring
+        or F.calculate(ctx)
 
-    return U.unwrap_cstr(cstr)
+    assert(inbuilt or (ctx.ctype ~= U.ctype.blockwise), {
+        msg = vim.bo.filetype .. " doesn't support block comments!",
+    })
+
+    -- 3. Last resort to use native commentstring
+    return U.unwrap_cstr(inbuilt or vim.bo.commentstring)
 end
 
 ---Returns a closure which is used to do comments