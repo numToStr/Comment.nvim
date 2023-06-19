370c350e35341f31b6e32465fd4e88f1cae251cc
Amirali Esmaeili <esmailiamirali@gmail.com>
2023-06-19 19:27:59 +0330

    Add rescript support

--- lua/Comment/ft.lua
+++ lua/Comment/ft.lua
@@ -118,6 +118,7 @@ local L = setmetatable({
     readline = { M.hash },
     rego = { M.hash },
     remind = { M.hash },
+    rescript = { M.cxx_l, M.cxx_b },
     robot = { M.hash }, -- Robotframework doesn't have block comments
     ruby = { M.hash },
     rust = { M.cxx_l, M.cxx_b },
